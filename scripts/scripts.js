28e05b978f93e4d23c6a98538dc941eae0f3ec59
uchtal <kerim.r.19@gmail.com>
2025-05-12 22:22:42 +0600

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,13 +9,51 @@ var swiper = new Swiper(".shop .mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   loop: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 });
 // End Shop Slider
+
+// GallerySlider
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  // autoplay: {
+  //   delay: 3500,
+  //   disableOnInteraction: false,
+  // },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  centeredSlides: true,
+  // autoplay: {
+  //   delay: 4500,
+  //   disableOnInteraction: false,
+  // },
+  breakpoints: {
+    640: {
+      slidesPerView: 2.01,
+    },
+    1200: {
+      slidesPerView: 4.01,
+    },
+  },
+});
+// End Gallery Slider
